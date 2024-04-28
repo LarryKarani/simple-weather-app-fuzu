0e477ac157025f6c7fab59a84633a2d2d8304d70
larry karani <larry@larrys-MacBook-Pro.local>
2024-04-28 15:38:08 +0300

    feat(styling): add global fonts and styling

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Weather from './componets/Weather';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
       <Weather/>
     </div>
   );
--- src/componets/Weather.jsx
+++ src/componets/Weather.jsx
@@ -2,36 +2,57 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Weather = () => {
-	const [city, setCity] = useState('');
-	const [weatherData, setWeatherData] = useState(null);
+    const [data, setData] = useState({});
+	const [location, setLocation] = useState('');
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`;
 
-	const fetchWeatherData = async () => {
-		try {
-			const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`);
-			setWeatherData(response.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		fetchWeatherData();
-	};
+    const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data);
+                console.log(response.data);
+            });
+            setLocation('');
+        }
+    };
 
 	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<input type='text' placeholder='Enter city name' value={city} onChange={(event) => setCity(event.target.value)} />
-				<button type='submit'>Get Weather</button>
-			</form>
-			{weatherData && (
-				<div>
-					<h2>{weatherData.name}</h2>
-					<p>{weatherData.weather[0].description}</p>
-					<p>Temperature: {weatherData.main.temp}</p>
+		<div className='app'>
+			<div className='search'>
+				<input
+					value={location}
+					onChange={(event) => setLocation(event.target.value)}
+					onKeyPress={searchLocation}
+					placeholder='Enter Location'
+					type='text'
+				/>
+			</div>
+			<div className='container'>
+				<div className='top'>
+					<div className='location'>
+						<p>{data.name}</p>
+					</div>
+					<div className='temp'>{data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}</div>
+					<div className='description'>{data.weather ? <p>{data.weather[0].main}</p> : null}</div>
 				</div>
-			)}
+
+				{data.name !== undefined && (
+					<div className='bottom'>
+						<div className='feels'>
+							{data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+							<p>Feels Like</p>
+						</div>
+						<div className='humidity'>
+							{data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+							<p>Humidity</p>
+						</div>
+						<div className='wind'>
+							{data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+							<p>Wind Speed</p>
+						</div>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
